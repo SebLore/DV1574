ce06f7e65a9f01c8882d40c0675f5d48bd3f2d3c
Sebastian Lorensson <34066570+SebLore@users.noreply.github.com>
2024-10-31 12:07:20 +0100

    Initial Commit

---
+++ Practice/circle_area.py
@@ -0,0 +1,5 @@
+import math
+pi = math.pi
+radius = 3
+area = pi*radius
+print(f'Circle Area: {pi*radius:.2f}')
---
+++ Practice/practice.py
@@ -0,0 +1,39 @@
+
+
+def add(x, y):
+    return x+y
+
+def mult(x, y):
+    print(x*y)
+
+def diff(a=0, b=0):
+     return a-b 
+
+add(1,2)
+print(add(2,3))
+mult(3,4)
+print(mult(4,5))
+print(diff(5))
+
+name = "Python"
+
+def pro():
+    name = "Python-pro"
+
+
+print(name)
+
+n = 4
+
+def four_power():
+    # n = n**4
+    print(n)
+
+four_power()
+
+def fun():
+    x=x+1
+    print(x)   
+
+x=12
+print(x)
